58354cb70025d8113fe5efa85df0ed715a83882d
rui <jianrui_android@163.com>
2018-02-14 12:25:53 +0800

    Please enter the commit message for your changes. Lines starting   with ' ' will be ignored, and an empty message aborts the commit.
      On branch master
      Your branch is up-to-date with 'origin/master'.
    
      Changes to be committed:
     	new file:   main/DeployDemo.go

---
+++ main/DeployDemo.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"strings"
+	"log"
+)
+
+/**
+部署go应用
+Supervisord
+Supervisord会帮你把管理的应用程序转化为daemon程序,并且可以通过命令执行开启，关闭，重启等操作
+并且崩溃后会自动重启,保证程序有自我修复功能
+ */
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()  //解析参数，默认是不会解析的
+	fmt.Println(r.Form)  //这些信息是输出到服务器端的打印信息
+	fmt.Println("path", r.URL.Path)
+	fmt.Println("scheme", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello astaxie!") //这个写入到w的是输出到客户端的
+}
+
+func main() {
+	http.HandleFunc("/", sayhelloName) //设置访问的路由
+	err := http.ListenAndServe(":9090", nil) //设置监听的端口
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
