18cab9547ace4a2343e911b0afb5337099a0bbe4
rui <jianrui_android@163.com>
2018-02-14 12:53:00 +0800

    Please enter the commit message for your changes. Lines starting   with ' ' will be ignored, and an empty message aborts the commit.
      On branch master
      Your branch is up-to-date with 'origin/master'.
    
      Changes to be committed:
     	modified:   DeployDemo.go

--- main/DeployDemo.go
+++ main/DeployDemo.go
@@ -24,7 +24,7 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
-	fmt.Fprintf(w, "然山海不可平") //这个写入到w的是输出到客户端的
+	fmt.Fprintf(w, "我爱黄涵") //这个写入到w的是输出到客户端的
 }
 
 func main() {
