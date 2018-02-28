9f9921e74a9c26013329621e4a51f3594be1170e
[rui] <[jianrui_android@163.com]>
2018-02-28 18:04:10 +0800

    Please enter the commit message for your changes. Lines starting   with ' ' will be ignored, and an empty message aborts the commit.   On branch master   Your branch is up-to-date with 'origin/master'.
      Changes to be committed:
     	modified:   .idea/workspace.xml
     	deleted:    main/GinDemo.go
     	modified:   main/main.go

--- main/main.go
+++ main/main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"fmt"
 	"strings"
+	"github.com/gin-gonic/gin"
 )
 
 /**
@@ -26,9 +27,32 @@ func hh(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", hh) //设置访问的路由
-	http.ListenAndServe(":80", nil) //设置监听的端口
+	/*http.HandleFunc("/", hh) //设置访问的路由
+	http.ListenAndServe(":80", nil) //设置监听的端口*/
 	//if err != nil {
 	//	log.Fatal("ListenAndServe: ", err)
 	//}
+
+	
+	router := gin.Default()
+	/*
+	router.GET("/", func(context *gin.Context) {
+		context.String(http.StatusOK,"gin demo is running")
+	})
+	*/
+	//restful路由
+	/*router.GET("/:user", func(context *gin.Context) {
+		user := context.Param("user")
+		context.String(http.StatusOK,"您好啊 %s",user)
+	})*/
+	//参数处理
+	router.GET("/user", func(context *gin.Context) {
+		name:=context.Query("name")
+		context.String(http.StatusOK,"fuck the %s",name)
+	})
+	/*	//body处理
+		router.GET("/user", func(context *gin.Context) {
+
+		})*/
+	router.Run(":80")
 }
