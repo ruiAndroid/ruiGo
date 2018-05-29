fae8898b5e3627a8af6f43cfc715c467ac7ae789
rui <jianrui_android@163.com>
2018-05-29 22:51:58 +0800

    Please enter the commit message for your changes. Lines starting   with ' ' will be ignored, and an empty message aborts the commit.
      On branch master
      Your branch is up-to-date with 'origin/master'.
    
      Changes to be committed:
     	modified:   src/http/controller/routes.go
    
      Untracked files:
     	C:/

--- src/http/controller/routes.go
+++ src/http/controller/routes.go
@@ -5,7 +5,7 @@ func RegisterRoutes(){
 	//注册首页相关路由
 	//new(IndexController).RegistRoute()
 	//注册收集用户bug路由
-	new(UserBugTrackerController).RegistRoute()
+	//new(UserBugTrackerController).RegistRoute()
 	//收集用户bug存储至mongo中
 	new(UserBugTrackerController).RegisterBugMsgCollectionWithMgo()
 }
